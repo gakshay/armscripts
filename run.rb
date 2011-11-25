318bc1770a706dccee559398dee3fc433bc2c95d
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-11-25 23:26:22 +0530

    changes in run.rb

--- run.rb
+++ run.rb
@@ -1,6 +1,19 @@
-#! /usr/bin/ruby
-require 'app/user.rb'
+re 'app/user.rb'
+require 'app/scan.rb'
+require 'app/send.rb'
 
-user = User.new
-user.login
+PATH="/home/root/files"
 
+scan = scan_files
+if scan == 1
+  user = User.new
+  response = user.login
+  if response 
+    user = Send.new("#{user.mobile}", "#{user.password}", PATH)
+    user.export
+  end
+end
+
+
+
+#  user = Send.new("9711335593", "1112")
